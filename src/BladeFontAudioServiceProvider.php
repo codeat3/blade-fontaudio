9f424aa8fa7b8d6af46511a6ae991ec145cf2792
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 01:15:33 +0530

    Added support for config file

--- src/BladeFontAudioServiceProvider.php
+++ src/BladeFontAudioServiceProvider.php
@@ -6,17 +6,25 @@ namespace Codeat3\BladeFontAudio;
 
 use BladeUI\Icons\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
 
 final class BladeFontAudioServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->callAfterResolving(Factory::class, function (Factory $factory) {
-            $factory->add('font-audio', [
-                'path' => __DIR__.'/../resources/svg',
-                'prefix' => 'fontaudio',
-            ]);
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-fontaudio', []);
+
+            $factory->add('font-audio', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
+
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-fontaudio.php', 'blade-fontaudio');
     }
 
     public function boot(): void
@@ -25,6 +33,10 @@ final class BladeFontAudioServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../resources/svg' => public_path('vendor/blade-fontaudio'),
             ], 'blade-fontaudio');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-fontaudio.php' => $this->app->configPath('blade-fontaudio.php'),
+            ], 'blade-fontaudio-config');
         }
     }
 }
